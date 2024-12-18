5a49bea648df2d76c46b8acedbdfd29648f0df63
Gehrig Dixon <78986974+Gurbiggg@users.noreply.github.com>
2024-12-17 18:37:53 -0700

    Im leaving to doom for a bit, signing off for now

--- init.el
+++ init.el
@@ -48,7 +48,7 @@
 (tool-bar-mode -1)
 (tooltip-mode -1)
 (set-fringe-mode 10)
-
+(menu-bar-mode -1)
 ;;(global-display-line-numbers-mode t)
 (setq display-line-numbers-type 'relative)
 ;;(dolist (mode '(org-mode-hook
@@ -60,7 +60,7 @@
 ;;  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 ;; Font
-(set-face-attribute 'default nil :font "Fira Code Retina" :height 90)
+(set-face-attribute 'default nil :font "Fira Code" :height 100)
 
 ;; Set ESC as a quit bind
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -77,6 +77,115 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(setq backup-directory-alist
+      `(("." . ,(concat user-emacs-directory "backups"))))
+
+;;; Themes
+;; Usable ones:
+;; ef-dream
+;; doom-ayu-mirage
+;; doom-dark+
+;; doom-challenger-deep
+;; doom-material-dark
+;; doom-one
+;; doom-tomorrow-night
+;; modus-vivendi
+(use-package ef-themes
+  :init (load-theme 'ef-dream t))
+  
+(use-package doom-themes)
+  ;;:config (load-theme 'doom-tomorrow-night t))
+;;(use-package color-theme-sanityinc-tomorrow)
+;;(load-theme 'doom-tomorrow-night t)
+
+
+;;defun efs/run-in-background (command)
+;; (let ((command-parts (split-string command "[ ]+")))
+;;   (apply #'call-process `(,(car command-parts) nil 0 nil ,@(cdr command-parts)))))
+;;
+;;defun dw/exwm-init-hook ()
+;; (exwm-workspace-switch-create 1)
+;; (efs/run-in-background "nm-applet"))
+;;
+;;defun efs/set-wallpaper ()
+;; (interactive)
+;; (start-prcoess-shell-command
+;;  "feh" nil "feh --bg-scale ~/Pictures/Wallpapers/ScarletTreeDark.png"))
+;;
+;;use-package exwm
+;; :init
+;; (setq mouse-autoselect-window t
+;;	focus-follows-mouse t)
+;; :config
+;; (setq exwm-workspace-show-all-buffers t)
+;; (setq exwm-layout-show-all-buffers t)
+;; (setq exwm-workspace-number 5)
+;; (setq exwm-input-prefix-keys
+;;	'(?\C-x
+;;	  ?\C-u
+;;	  ?\C-h
+;;	  ?\M-x
+;;	  ?\M-`
+;;	  ?\M-&
+;;	  ?\M-:
+;;	  ?\C-\M-j
+;;	  ?\C-\ ))
+;;
+;; (define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
+;;
+;; (setq exwm-input-global-keys
+;;	`(
+;;	  ([?\s-r] . exwm-reset)
+;;	  ([s-h] . windmove-left)
+;;	  ([s-j] . windmove-down)
+;;	  ([s-k] . windmove-up)
+;;	  ([s-l] . windmove-right)
+;;
+;;	  ([?\s-&] . (lambda (command)
+;;		       (interactive (list (read-shell-command "$ ")))
+;;		       (start-process-shell-command command nil command)))
+;;	  
+;;	  ([?\s-w] . exwm-workspace-switch)
+;;
+;;	  ,@(mapcar (lambda (i)
+;;		      `(,(kbd (format "s-%d" i)) .
+;;			(lambda ()
+;;			  (interactive)
+;;			  (exwm-workspace-switch-create ,i))))
+;;		    (number-sequence 0 9))))
+;; 
+;; (add-hook 'exwm-update-class-hook
+;;	    (lambda ()
+;;	      (exwm-workspace-rename-buffer exwm-class-name)))
+;; (setq exwm-input-prefix-keys '())
+;;
+;; (set-frame-parameter (selected-frame) 'alpha '(90 . 90))
+;; (add-to-list 'default-frame-alist '(alpha . (90 . 90)))
+;; (set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+;; (add-to-list 'default-frame-alist '(fullscreen . maximized))
+;; 
+;; (require 'exwm-randr)
+;; (exwm-randr-mode)
+;; 
+;; (start-process-shell-command "xrandr" nil "")
+;; (efs/set-wallpaper)
+;; 
+;; (require 'exwm-systemtray)
+;; (exwm-systemtray-mode)
+;; (add-hook 'exwm-init-hook #'efs/after-exwm-init)
+;; 
+;; (exwm-enable))
+;;
+;;
+;;use-package desktop-environment
+;; :after exwm
+;; :config (desktop-environment-mode)
+;; :custom
+;; (desktop-environment-brightness-small-increment "2%+")
+;; (desktop-environment-brightness-small-decrement "2%+")
+;; (desktop-environment-brightness-normal-increment "5%+")
+;; (desktop-environment-brightness-normal-decrement "5%+"))
+    
 (defun efs/org-mode-setup ()
   (org-indent-mode)
   (variable-pitch-mode 1)
@@ -130,29 +239,9 @@
 (use-package visual-fill-column
   :hook (org-mode . efs/org-mode-visual-fill))
 
-
 ;;(use-package no-littering)
 ;;(setq auto-save-file-name-transforms
 ;;      `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
-
-(setq backup-directory-alist
-      `(("." . ,(concat user-emacs-directory "backups"))))
-
-;;; Themes
-;; Usable ones:
-;; ef-dream
-;; doom-ayu-mirage
-;; doom-dark+
-;; doom-challenger-deep
-;; doom-material-dark
-;; doom-one
-;; doom-tomorrow-night
-;; modus-vivendi
-(use-package ef-themes
-  ;;:init (load-theme 'ef-dream t)
-  )
-(use-package doom-themes
-  :init (load-theme 'doom-tomorrow-night t))
 
 ;; completion engine/better minibuffer
 (use-package vertico
@@ -205,7 +294,7 @@
 
 (use-package doom-modeline
   :init (doom-modeline-mode 1)
-  :custom (doom-modeline-height 20))
+  :custom (doom-modeline-height 40))
 
 (use-package nerd-icons)
 
@@ -217,19 +306,42 @@
 (use-package vterm
   :commands vterm
   :config
-  (setq vterm-shell "fish")
+  (setq vterm-shell "zsh")
   (setq vterm-max-scrollback 10000))
+
+;; eldoc
+(use-package eldoc
+  :ensure (:wait t)
+  :init
+  (global-eldoc-mode))
+
+;; jsonrpc dep
+(use-package jsonrpc)
+
+;; lsp mode
+(use-package eglot
+  :hook
+  (rust-mode . eglot-ensure))
+
+;; to make it faster...
+;;use-package eglot-booster
+;; :after eglot
+;; :config (eglot-booster-mode))
 
 ;; rust mode
 (use-package rust-mode
-;;  :init 
-;;  (setq rust-mode-treesitter-derive t))
-)
+  :init 
+  (setq rust-mode-treesitter-derive t))
+
+;; nix mode
+(use-package nix-ts-mode
+  :mode "\\.nix\\'")
 
 ;; clojure mode
 (use-package clojure-ts-mode)
 
 ;;(use-package magit)
+
 (use-package dashboard
   :config
   (add-hook 'elpaca-after-init-hook #'dashboard-insert-startupify-lists)
@@ -263,3 +375,98 @@
   (setq dashboard-icon-type 'nerd-icons)
   (setq dashboard-set-heading-icons t)
   (setq dashboard-set-file-icons t))
+ 
+(defun meow-setup ()
+  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
+  (meow-motion-overwrite-define-key
+   '("j" . meow-next)
+   '("k" . meow-prev)
+   '("<escape>" . ignore))
+  (meow-leader-define-key
+   ;; SPC j/k will run the original command in MOTION state.
+   '("j" . "H-j")
+   '("k" . "H-k")
+   ;; Use SPC (0-9) for digit arguments.
+   '("1" . meow-digit-argument)
+   '("2" . meow-digit-argument)
+   '("3" . meow-digit-argument)
+   '("4" . meow-digit-argument)
+   '("5" . meow-digit-argument)
+   '("6" . meow-digit-argument)
+   '("7" . meow-digit-argument)
+   '("8" . meow-digit-argument)
+   '("9" . meow-digit-argument)
+   '("0" . meow-digit-argument)
+   '("/" . meow-keypad-describe-key)
+   '("?" . meow-cheatsheet)
+   ;; Use SPC . to open files
+   '("." . find-file)
+   '("s" . save-buffer))
+  (meow-normal-define-key
+   '("0" . meow-expand-0)
+   '("9" . meow-expand-9)
+   '("8" . meow-expand-8)
+   '("7" . meow-expand-7)
+   '("6" . meow-expand-6)
+   '("5" . meow-expand-5)
+   '("4" . meow-expand-4)
+   '("3" . meow-expand-3)
+   '("2" . meow-expand-2)
+   '("1" . meow-expand-1)
+   '("-" . negative-argument)
+   '(";" . meow-reverse)
+   '("," . meow-inner-of-thing)
+   '("." . meow-bounds-of-thing)
+   '("[" . meow-beginning-of-thing)
+   '("]" . meow-end-of-thing)
+   '("a" . meow-append)
+   '("A" . meow-open-below)
+   '("b" . meow-back-word)
+   '("B" . meow-back-symbol)
+   '("c" . meow-change)
+   '("d" . meow-delete)
+   '("D" . meow-backward-delete)
+   '("e" . meow-next-word)
+   '("E" . meow-next-symbol)
+   '("f" . meow-find)
+   '("g" . meow-cancel-selection)
+   '("G" . meow-grab)
+   '("h" . meow-left)
+   '("H" . meow-left-expand)
+   '("i" . meow-insert)
+   '("I" . meow-open-above)
+   '("j" . meow-next)
+   '("J" . meow-next-expand)
+   '("k" . meow-prev)
+   '("K" . meow-prev-expand)
+   '("l" . meow-right)
+   '("L" . meow-right-expand)
+   '("m" . meow-join)
+   '("n" . meow-search)
+   '("o" . meow-block)
+   '("O" . meow-to-block)
+   '("p" . meow-yank)
+   '("q" . meow-quit)
+   '("Q" . meow-goto-line)
+   '("r" . meow-replace)
+   '("R" . meow-swap-grab)
+   '("s" . meow-kill)
+   '("t" . meow-till)
+   '("u" . meow-undo)
+   '("U" . meow-undo-in-selection)
+   '("v" . meow-visit)
+   '("w" . meow-mark-word)
+   '("W" . meow-mark-symbol)
+   '("x" . meow-line)
+   '("X" . meow-goto-line)
+   '("y" . meow-save)
+   '("Y" . meow-sync-grab)
+   '("z" . meow-pop-selection)
+   '("'" . repeat)
+   '("<escape>" . ignore)))
+
+(use-package meow
+  :init
+  (require 'meow)
+  (meow-setup)
+  (meow-global-mode 1))
